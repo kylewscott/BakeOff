ed729ef3f18e24533a4c6e6251631eebb556cdc1
Kyle Scott <98427493+kylewscott@users.noreply.github.com>
2024-12-04 23:24:54 -0600

    Fix spacing

--- bakeOff.c
+++ bakeOff.c
@@ -21,7 +21,7 @@
 // Ingredients
 typedef enum 
 {
-	FLOUR, SUGAR, YEAST, BAKING_SODA, SALT, CINNAMON,   // Pantry ingredients
+    FLOUR, SUGAR, YEAST, BAKING_SODA, SALT, CINNAMON,   // Pantry ingredients
     EGG, MILK, BUTTER,                                  // Refrigerator ingredients
     INGREDIENT_COUNT
 } Ingredient;
@@ -29,7 +29,7 @@ typedef enum
 // Recipes
 typedef enum 
 {
-	COOKIES,
+    COOKIES,
     PANCAKES,
     PIZZA_DOUGH,
     SOFT_PRETZELS,
@@ -55,7 +55,7 @@ char* ingredient_names[] =
 // Ingredient requirements for each recipe
 Ingredient recipe_ingredients[][8] = 
 {
-	[COOKIES] = {FLOUR, SUGAR, MILK, BUTTER, -1},
+    [COOKIES] = {FLOUR, SUGAR, MILK, BUTTER, -1},
     [PANCAKES] = {FLOUR, SUGAR, BAKING_SODA, SALT, EGG, MILK, BUTTER, -1},
     [PIZZA_DOUGH] = {YEAST, SUGAR, SALT, -1},
     [SOFT_PRETZELS] = {FLOUR, SUGAR, SALT, YEAST, BAKING_SODA, EGG, -1},
@@ -65,7 +65,7 @@ Ingredient recipe_ingredients[][8] =
 // Shared kitchen resources
 typedef struct 
 {
-	sem_t pantry;              // Binary semaphore for pantry access
+    sem_t pantry;              // Binary semaphore for pantry access
     sem_t refrigerators[2];    // Binary semaphores for refrigerators
     sem_t mixers;              // Counting semaphore for mixers
     sem_t bowls;               // Counting semaphore for bowls
@@ -83,7 +83,7 @@ typedef struct
 // Baker thread data
 typedef struct 
 {
-	int id;
+    int id;
     char* color;
     KitchenResources* kitchen;
 } Baker;
@@ -106,7 +106,7 @@ void ramsay_interrupt(Baker* baker);
 // Initialize kitchen resources
 void initialize_kitchen(int num_bakers) 
 {
-	// Initialize semaphores
+    // Initialize semaphores
     sem_init(&kitchen.pantry, 0, 1);
     sem_init(&kitchen.refrigerators[0], 0, 1);
     sem_init(&kitchen.refrigerators[1], 0, 1);
@@ -117,7 +117,7 @@ void initialize_kitchen(int num_bakers)
 
     // Initialize ingredient locks
     for (int i = 0; i < INGREDIENT_COUNT; i++) 
-	{
+    {
         sem_init(&kitchen.ingredient_locks[i], 0, 1);
         kitchen.ingredient_available[i] = 1;  // All ingredients are initially available
     }
@@ -131,17 +131,17 @@ void initialize_kitchen(int num_bakers)
 // Baker thread main function
 void* baker_thread(void* arg) 
 {
-	Baker* baker = (Baker*)arg;
+    Baker* baker = (Baker*)arg;
     
     // Attempt each recipe
     for (Recipe recipe = 0; recipe < RECIPE_COUNT; recipe++) 
-	{
+    {
         // Try to acquire ingredients
         printf("%sBaker %d is attempting to make %s%s\n", baker->color, baker->id, recipe_names[recipe], RESET);
         
         if (try_acquire_ingredients(baker, recipe)) 
-		{
-			mix_ingredients(baker, recipe);
+	{
+	    mix_ingredients(baker, recipe);
             release_ingredients(baker, recipe);
             bake_recipe(baker, recipe);
             
@@ -155,11 +155,11 @@ void* baker_thread(void* arg)
 // Simulate Gordon Ramsay 
 void ramsay_interrupt(Baker* baker) 
 {
-	printf("%s**GORDON RAMSAY INTERRUPTION FOR BAKER %d!**%s\n", baker->color, baker->id, RESET);
+    printf("%s**GORDON RAMSAY INTERRUPTION FOR BAKER %d!**%s\n", baker->color, baker->id, RESET);
     
-	// Release all currently held semaphores
+    // Release all currently held semaphores
     for (int i = 0; i < INGREDIENT_COUNT; i++)
-	{
+    {
         if (kitchen.ingredient_available[i] == 0)
         {
             kitchen.ingredient_available[i]++;
@@ -167,27 +167,27 @@ void ramsay_interrupt(Baker* baker)
         }
      }
     
-	sem_post(&kitchen.pantry);
+    sem_post(&kitchen.pantry);
     sem_post(&kitchen.refrigerators[0]);
     sem_post(&kitchen.refrigerators[1]);
-   	sem_post(&kitchen.mixers);
-   	sem_post(&kitchen.bowls);
-   	sem_post(&kitchen.spoons);
-   	sem_post(&kitchen.ovens);
+    sem_post(&kitchen.mixers);
+    sem_post(&kitchen.bowls);
+    sem_post(&kitchen.spoons);
+    sem_post(&kitchen.ovens);
 }
 
 // Main function with thread creation
 int main() 
 {
-	int num_bakers;
-   	printf("Enter number of bakers (max %d): ", MAX_BAKERS);
-   	scanf("%d", &num_bakers);
+    int num_bakers;
+    printf("Enter number of bakers (max %d): ", MAX_BAKERS);
+    scanf("%d", &num_bakers);
 
-   	if (num_bakers < 1 || num_bakers > MAX_BAKERS) 
-	{
+    if (num_bakers < 1 || num_bakers > MAX_BAKERS) 
+    {
        	printf("Invalid number of bakers. Must be between 1 and %d\n", MAX_BAKERS);
        	return 1;
-   	}
+    }
 
     // Baker color array
     char* baker_colors[] = {RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN};
@@ -199,23 +199,23 @@ int main()
     pthread_t baker_threads[MAX_BAKERS];
     Baker bakers[MAX_BAKERS];
 
-   	for (int i = 0; i < num_bakers; i++) 
-	{
+    for (int i = 0; i < num_bakers; i++) 
+    {
        	bakers[i].id = i;
        	bakers[i].color = baker_colors[i % 6];
        	bakers[i].kitchen = &kitchen;
         
        	pthread_create(&baker_threads[i], NULL, baker_thread, &bakers[i]);
-   	}
+    }
 
     // Wait for all baker threads to complete
     for (int i = 0; i < num_bakers; i++) 
-	{
+    {
        	pthread_join(baker_threads[i], NULL);
-   	}
+    }
 
-   	printf("\n All bakers have finished their recipes! \n");
-   	return 0;
+    printf("\n All bakers have finished their recipes! \n");
+    return 0;
 }
 
 int try_acquire_ingredients(Baker* baker, Recipe recipe) 
@@ -260,7 +260,7 @@ void release_ingredients(Baker* baker, Recipe recipe)
 {
     printf("%sBaker %d is trying to release all ingredients for %s%s\n", baker->color, baker->id, recipe_names[recipe], RESET);
 
-	for(int i = 0; recipe_ingredients[recipe][i] != -1; i++) 
+    for(int i = 0; recipe_ingredients[recipe][i] != -1; i++) 
     {
         Ingredient ingredient = recipe_ingredients[recipe][i];
         sem_wait(&kitchen.ingredient_locks[ingredient]);
@@ -276,27 +276,27 @@ void mix_ingredients(Baker* baker, Recipe recipe)
 	// Acquire mixer, bowl, and spoon
     sem_wait(&kitchen.mixers);
     sem_wait(&kitchen.bowls);
-   	sem_wait(&kitchen.spoons);
+    sem_wait(&kitchen.spoons);
 
-   	printf("%sBaker %d is mixing ingredients for %s%s\n", baker->color, baker->id, recipe_names[recipe], RESET);
+    printf("%sBaker %d is mixing ingredients for %s%s\n", baker->color, baker->id, recipe_names[recipe], RESET);
     
-   	sleep(1);  // Simulate mixing time
+    sleep(1);  // Simulate mixing time
 
-   	// Release mixer, bowl, and spoon
-   	sem_post(&kitchen.mixers);
-   	sem_post(&kitchen.bowls);
-   	sem_post(&kitchen.spoons);
+    // Release mixer, bowl, and spoon
+    sem_post(&kitchen.mixers);
+    sem_post(&kitchen.bowls);
+    sem_post(&kitchen.spoons);
 }
 
 void bake_recipe(Baker* baker, Recipe recipe) 
 {
-	// Acquire oven
-   	sem_wait(&kitchen.ovens);
+    // Acquire oven
+    sem_wait(&kitchen.ovens);
 
-   	printf("%sBaker %d is baking %s%s\n", baker->color, baker->id, recipe_names[recipe], RESET);
+    printf("%sBaker %d is baking %s%s\n", baker->color, baker->id, recipe_names[recipe], RESET);
     
-   	sleep(2);  // Simulate baking time
+    sleep(2);  // Simulate baking time
 
-   	// Release oven
-   	sem_post(&kitchen.ovens);
+    // Release oven
+    sem_post(&kitchen.ovens);
 }
